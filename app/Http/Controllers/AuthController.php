d2834b670e5226e602b294305f39f1c197689b57
NurulH123 <hidayatnurul937@gmail.com>
2024-02-27 17:37:42 +0700

    revisi /me

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,6 +18,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
+    protected $units = 0;
+    protected $vendor = 0;
+    protected $role = 0;
+    protected $position = 0;
+    protected $category = 0;
+    protected $status_employee = 0;
+    protected $customer = 0;
+
     public function register(Request $request)
     {
         $validator =  Validator::make(
@@ -187,18 +195,22 @@ class AuthController extends Controller
 
     private function dataMaster()
     {
-        $user = auth()->user();
+        $checkEmployee = is_null(auth()->user()->employee);
+        $user = $checkEmployee ? 
+                    auth()->user() : auth()->user()->employee;
+                    // $user = auth()->user()->employee;
+                    // dd($user);
         $company = $user->company;
 
-        return [
-            'unit' => !is_null($company) ? (!is_null($company->units) ? count($company->units) : 0) : 0,
-            'vendor' => !is_null($company) ? (!is_null($company->vendor) ? count($company->vendor) : 0) : 0,
-            'role' => !is_null($company) ? (!is_null($company->roles) ? count($company->roles) : 0) : 0,
-            'position' => !is_null($company) ? (!is_null($company->positions) ? count($company->positions) : 0) : 0,
-            'category' => !is_null($company) ? (!is_null($company->productCategories) ? count($company->productCategories) : 0) : 0,
-            'warehouse' => !is_null($company) ? (!is_null($company->warehouses) ? count($company->warehouses) : 0) : 0,
-            'status_employee' => !is_null($company) ? (!is_null($company->employeeStatus) ? count($company->employeeStatus) : 0) : 0,
-            'customer' => !is_null($company) ? (!is_null($company->customers) ? count($company->customers) : 0) : 0
+        return $checkEmployee ? null : [
+            'unit' => !is_null($company->units) ? count($company->units) : 0,
+            'vendor' =>  !is_null($company->vendor) ? count($company->vendor) : 0,
+            'role' =>  !is_null($company->roles) ? count($company->roles) : 0,
+            'position' =>  !is_null($company->positions) ? count($company->positions) : 0,
+            'category' =>  !is_null($company->productCategories) ? count($company->productCategories) : 0,
+            'warehouse' =>  !is_null($company->warehouses) ? count($company->warehouses) : 0,
+            'status_employee' =>  !is_null($company->employeeStatus) ? count($company->employeeStatus) : 0,
+            'customer' =>  !is_null($company->customers) ? count($company->customers) : 0
         ];
 
     }
