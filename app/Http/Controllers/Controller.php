4be413f741b379148a426ce0188614ff0b81f046
NurulH123 <hidayatnurul937@gmail.com>
2024-03-03 17:19:15 +0700

    delete model cache

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,18 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Customer;
-use App\Models\Position;
-use App\Models\ProductCategory;
-use App\Models\Role;
-use App\Models\StatusEmployee;
-use App\Models\Unit;
-use App\Models\Vendor;
-use App\Models\Warehouse;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Symfony\Component\HttpFoundation\Response;
 
 class Controller extends BaseController
 {
